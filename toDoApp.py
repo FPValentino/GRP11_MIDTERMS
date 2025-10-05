1858811c173df1c4b38224e77c1109ce1acdd5b3
ADTQue <queadrian21a@gmail.com>
2025-10-05 15:51:12 +0800

    Add File Handling , Edit Menu

--- toDoApp.py
+++ toDoApp.py
@@ -1,39 +1,80 @@
+# currently implemented standards:
+# 1. Docstring for functions
+# 2. Indentation of code blocks
+# 3. Implementing naming convention (snake_case for functions and variables)
+
 # toDoApp.py
+import os
 
 tasks=[]
+TASKS_FILE= "tasks.txt" #UPPERCASE to indicate a constant as mentioned by pylint
 
-def addtask(task) :
-  tasks.append(task)
-  print("task added!")
-
-def showTasks( ):
-    if len(tasks)==0 :
-      print("no tasks yet")
+def load_tasks():
+    """Load tasks from file if it exists.
+    """
+    global tasks
+    if os.path.exists(TASKS_FILE):
+        with open(TASKS_FILE, "r", encoding="utf-8") as f:
+            tasks = [line.strip() for line in f.readlines()]
     else:
-     for i in range (len(tasks)):
-      print(i+1,".",tasks[i])
+        tasks = []
 
-def removetask(tasknumber):
-    tasks.pop(tasknumber) 
+def save_tasks():
+    """Save tasks to file.
+    """
+    with open(TASKS_FILE, "w", encoding="utf-8") as f:
+        for task in tasks:
+            f.write(task + "\n")
+
+def add_task(task) :
+    """Add a new task to the task list.
+
+    Args:
+        task (str): The task to be added.
+    """
+    tasks.append(task)
+    save_tasks()
+    print("task added!")
+
+def show_tasks():
+    """Shows all tasks in the task list.
+    """
+    if len(tasks)==0 :
+        print("no tasks yet")
+    else:
+        for i in range (len(tasks)):
+            print(i+1,".",tasks[i])
+
+def remove_task(tasknumber):
+    """Removes task from the task list.
+
+    Args:
+        tasknumber (int): The index of the task to be removed (1-based index).
+    """
+    tasks.pop(tasknumber)
     print("task removed!!")
 
 def main():
+    """The main function to run the to-do app.
+    """
+    load_tasks()
     while True:
-        print("1 Add Task")
-        print("2.Show Tasks")
-        print("3.Remove Task")
-        print("4- Exit")
+        print("1.| Add Task   |")
+        print("2.| Show Tasks |")
+        print("3.| Remove Task|")
+        print("4.| Exit       |")
         ch = input("enter choice : ")
         if ch=="1":
             t = input("enter task : ")
-            addtask(t)
+            add_task(t)
         elif ch=="2":
-            showTasks()
+            show_tasks()
         elif ch=="3":
             n=int(input("enter task no to remove: "))
-            removetask(n)   
+            remove_task(n)
         elif ch=="4":
-            break;
+            print("Exiting program")
+            break
         else:
             print("wrong choice!!")
 main()
